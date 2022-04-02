87c2156796121dec320f66258bc40e0281287271
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-02 19:23:20 -0300

    Create flexColCenter mixin

---
+++ src/styles/mixins.js
@@ -0,0 +1,10 @@
+import { themeBase } from "./variables";
+
+export const flexColCenter = () => {
+  return `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  `
+}
