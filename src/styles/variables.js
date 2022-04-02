8879f852c882744635c377a85a5f80048cf3d419
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-02 19:05:32 -0300

    Create variables styles

---
+++ src/styles/variables.js
@@ -0,0 +1,33 @@
+export const themeBase = {
+  mainColor: '#A43CD4',
+  secondaryFont: 'Exo 2',
+
+  buttonColor: '#A43CD4',
+  buttonColorHover: '#7B2B9F',
+  buttonStopColor: '#ED2424',
+  buttonStopColorHover: '#C61616',
+
+  popupEditBgColor: '#9DF1A0',
+  popupDeleteBgColor: '#F1B19D',
+  popupConfirmBgColor: '#E5E5E5',
+
+  dropShadow: '0px 8px 12px rgba(0, 0, 0, 0.25)',
+  borderRadius: '4px',
+  transition: 'all .4s ease-out',
+  opacityBg: 'rgba(0, 0, 0, 0.8)'
+}
+
+export const tagColor = {
+  color1: '#55B859',
+  color2: '#55B2B8',
+  color3: '#555FB8',
+  color4: '#AB55B8',
+  color5: '#B8B455',
+  color6: '#B8555B',
+}
+
+export const priorityColor = {
+  color1: '#28ED24',
+  color2: '#ED2424',
+  color3: '#ED9C24',
+}
