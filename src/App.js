ea4e08335673c770acebc6c1edb2002e8ebcc7de
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-14 14:28:48 -0300

    Just updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import NewTaskForm from './components/NewTaskForm';
-import { Title } from './components/Title/style';
+
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <GlobalStyles />
       <Header />
-      <Title>Teste</Title>
+
       <NewTaskForm />
     </div>
   );
